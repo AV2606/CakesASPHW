1d210014d0c31840652d14c83e5809865ea402d6
Avichay Vaknin <79922086+AV2606@users.noreply.github.com>
2022-05-15 23:24:50 +0300

    Add project files.

---
+++ Cakes/Migrations/20220515200301_initial0.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cakes.Migrations
+{
+    public partial class initial0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cakes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Recipe = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cakes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cakes");
+        }
+    }
+}
---
+++ Cakes/Models/Cake.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cakes.Models
+{
+    public class Cake
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string  Recipe { get; set; }
+    }
+}
